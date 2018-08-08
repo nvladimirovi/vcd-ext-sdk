c32e38c34d0482fdc97590114695a1d274af0c7f
Nikola Vladimirov Iliev <nvladimirovi@vmware.com>
2018-08-08 14:57:21 +0300

    Implement change object key property name function
    This function is used to rename list of keys
    and copy the others into the new object.
    This function is used when the request for
    all plugins is made, rename the props
    , recived from the server, to be exactly
    like these in the uipluginmetadataresponse
    interface.
    Signed-off-by: Nikola Vladimirov Iliev <nvladimirovi@vmware.com>

--- ui/plugin-lifecycle/src/main/helpers/object-helpers.ts
+++ ui/plugin-lifecycle/src/main/helpers/object-helpers.ts
@@ -4,3 +4,13 @@ export function createStringEnum<T extends string>(keys: T[]): {[K in T]: K} {
         return res;
     }, Object.create(null));
 }
+
+export const renameProp = (oldKeys: string[], newKeys: string[], obj: any): any => {
+    oldKeys.forEach((oldkey: string, index: number) => {
+        obj[newKeys[index]] = obj[oldkey];
+        delete obj[oldkey];
+    });
+
+    return obj;
+};
+
--- ui/plugin-lifecycle/src/main/services/plugin-manager.service.ts
+++ ui/plugin-lifecycle/src/main/services/plugin-manager.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Inject } from "@angular/core";
 import { Http, Response, Headers, RequestOptions } from "@angular/http";
 import { Observable, BehaviorSubject } from "rxjs";
-import { UploadPayload, ChangeScopePlugin } from "../interfaces/Plugin";
+import { UploadPayload, ChangeScopePlugin, PluginResponse } from "../interfaces/Plugin";
 import { PluginValidator } from "../classes/plugin-validator";
 import { ScopeFeedback } from "../classes/ScopeFeedback";
 import { DisableEnablePluginService } from "./disable-enable-plugin.service";
@@ -9,9 +9,10 @@ import { PluginUploaderService } from "./plugin-uploader.service";
 import { DeletePluginService } from "./delete-plugin.service";
 import { PluginPublisher } from "./plugin-publisher.service";
 import { ChangeScopeRequestTo } from "../interfaces/ChangeScopeRequestTo";
-import {HttpTransferService} from "@vcd/http-transfer-service";
+import { HttpTransferService } from "@vcd/http-transfer-service";
 import { API_ROOT_URL, AuthTokenHolderService } from "@vcd-ui/common";
 import { UiPluginMetadataResponse } from "@vcd/bindings/vcloud/rest/openapi/model";
+import { renameProp } from "../helpers/object-helpers";
 
 @Injectable()
 export class PluginManager {
@@ -23,7 +24,7 @@ export class PluginManager {
     constructor(
         @Inject(API_ROOT_URL) private _baseUrl: string = "",
         private http: Http,
-        private authService: AuthTokenHolderService ,
+        private authService: AuthTokenHolderService,
         private disableEnablePlugin: DisableEnablePluginService,
         private pluginUploaderService: PluginUploaderService,
         private deletePluginService: DeletePluginService,
@@ -142,7 +143,19 @@ export class PluginManager {
         const promise = new Promise<void>((resolve, reject) => {
             this.reqPlugins().toPromise()
                 .then((res: Response) => {
-                    this._plugins = res.json();
+                    this._plugins = [];
+                    const resPlugins: PluginResponse[] = res.json();
+
+                    if (resPlugins.length > 0) {
+                        resPlugins.forEach((resPlugin: PluginResponse) => {
+                            const plugin: UiPluginMetadataResponse = renameProp(
+                                ["provider_scoped", "tenant_scoped"], ["providerScoped", "tenantScoped"],
+                                resPlugin
+                            );
+                            this._plugins.push(plugin);
+                        });
+                    }
+
                     this._pluginsSubject.next(this._plugins);
                     resolve();
                 })
@@ -168,57 +181,57 @@ export class PluginManager {
         const observable = new Observable<ChangeScopeRequestTo[]>((observer) => {
             // Extract useful data from the manifest
             this.pluginUploaderService.proccessManifest(payload.manifest)
-            .then((pluginDesc) => {
-                // Create headers
-                const headers = new Headers();
-                headers.append("Accept", "application/json");
-                headers.append("Content-Type", "application/json");
-                headers.append("x-vcloud-authorization", this.authService.token);
-                const opts = new RequestOptions();
-                opts.headers = headers;
+                .then((pluginDesc) => {
+                    // Create headers
+                    const headers = new Headers();
+                    headers.append("Accept", "application/json");
+                    headers.append("Content-Type", "application/json");
+                    headers.append("x-vcloud-authorization", this.authService.token);
+                    const opts = new RequestOptions();
+                    opts.headers = headers;
 
-                // Register plugin into the system
-                return this.http.post(`${this._baseUrl}/cloudapi/extensions/ui`, pluginDesc, opts).toPromise();
-            })
-            .then((registerPluginResponse: Response) => {
-                // Validate and Handle success
-                const RES = registerPluginResponse.json();
-                PLUGIN.id = RES.id;
-                PLUGIN.file = payload.file;
-                // Register the plugin name and size
-                return this.pluginUploaderService.enablePluginUpload(PLUGIN, this._baseUrl);
-            })
-            .then((enableResponse: Response) => {
-                // Send transfer link in the reslove where the plugin will be uploaded.
-                const linkHeader: string = enableResponse.headers.get("Link");
-                const url: string = linkHeader.split(">;")[0];
-                const transferLink = url.slice(1, url.length);
+                    // Register plugin into the system
+                    return this.http.post(`${this._baseUrl}/cloudapi/extensions/ui`, pluginDesc, opts).toPromise();
+                })
+                .then((registerPluginResponse: Response) => {
+                    // Validate and Handle success
+                    const RES = registerPluginResponse.json();
+                    PLUGIN.id = RES.id;
+                    PLUGIN.file = payload.file;
+                    // Register the plugin name and size
+                    return this.pluginUploaderService.enablePluginUpload(PLUGIN, this._baseUrl);
+                })
+                .then((enableResponse: Response) => {
+                    // Send transfer link in the reslove where the plugin will be uploaded.
+                    const linkHeader: string = enableResponse.headers.get("Link");
+                    const url: string = linkHeader.split(">;")[0];
+                    const transferLink = url.slice(1, url.length);
 
-                const headers = {
-                    "x-vcloud-authorization": this.authService.token
-                };
+                    const headers = {
+                        "x-vcloud-authorization": this.authService.token
+                    };
 
-                // Upload the plugin chunk by chunk
-                return this.httpTransferService.upload(headers, { file: payload.file, url: transferLink }).toPromise();
-            })
-            .then(() => {
-                // Publish the plugin if this kind of data is provided
-                if (scopeFeedback.forAllOrgs) {
-                    observer.next(this.pluginPublisher.publishPluginForAllTenants([PLUGIN], this.baseUrl, false));
-                    return;
-                }
+                    // Upload the plugin chunk by chunk
+                    return this.httpTransferService.upload(headers, { file: payload.file, url: transferLink }).toPromise();
+                })
+                .then(() => {
+                    // Publish the plugin if this kind of data is provided
+                    if (scopeFeedback.forAllOrgs) {
+                        observer.next(this.pluginPublisher.publishPluginForAllTenants([PLUGIN], this.baseUrl, false));
+                        return;
+                    }
 
-                if (scopeFeedback.data.length > 0) {
-                    const publishFor = this.handleMixedScope([{ id: PLUGIN.id, pluginName: payload.manifest.name }], scopeFeedback, false);
-                    observer.next(publishFor);
-                    return;
-                }
+                    if (scopeFeedback.data.length > 0) {
+                        const publishFor = this.handleMixedScope([{ id: PLUGIN.id, pluginName: payload.manifest.name }], scopeFeedback, false);
+                        observer.next(publishFor);
+                        return;
+                    }
 
-                observer.next();
-            })
-            .catch(err => {
-                observer.error(err);
-            });
+                    observer.next();
+                })
+                .catch(err => {
+                    observer.error(err);
+                });
         });
 
         return observable;
